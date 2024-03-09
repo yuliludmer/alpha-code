fccc9d6288539cdffd3e4178b98785615ef364c4
Yuli Ludmer <yuliludmer@Yulis-MacBook-Air.local>
2024-03-09 20:33:47 +0200

    functions right

--- main.py
+++ main.py
@@ -26,16 +26,17 @@ sin_zeta = np.zeros(shape)
 for i in range(len(phi)): #run on every phi dot
   for k in range(len(theta)): #run on evry theta dot
     cos_zeta[i,k] = np.cos(theta_obs)*np.cos(theta[k])+np.sin(theta_obs)*np.sin(theta[k])*np.sin(phi[i]) #zeta formula (changed)
-    sin_zeta[i,k] = math.sqrt(1-(cos_zeta[i, k])**2)
+    sin_zeta[i,k] = np.sqrt(1-(cos_zeta[i, k])**2)
     #if (zeta[i,k]<0):
      #print(zeta[i,k])
+
 '''
-sin_mask = sin_zeta >= (theta_gamma) #ask gilad
-cos_mask = cos_zeta >= (theta_gamma)
+sin_mask = sin_zeta >= np.sin(theta_gamma) #ask gilad
+cos_mask = cos_zeta >= np.cos(theta_gamma)
 sin_zeta_beaming = np.copy(sin_zeta)
 cos_zeta_beaming = np.copy(cos_zeta)
-sin_zeta_beaming = np.ma.masked_array(np.abs(sin_zeta), sin_mask)
-cos_zeta_beaming = np.ma.masked_array(np.abs(cos_zeta), cos_mask)
+sin_zeta_beaming = np.ma.masked_array(sin_zeta, sin_mask)
+cos_zeta_beaming = np.ma.masked_array(cos_zeta, cos_mask)
 #print(zeta_beaming<0)
 '''
 R = np.sqrt(1-(1/gamma**2))*c*t_0 #R formula
@@ -51,8 +52,8 @@ x = np.zeros(shape)
 y = np.zeros(shape)
 for i in range(len(x)): #run on every phi dot
   for k in range(len(y)): #run on evry theta dot
-    cos_psi[i,k] = (-np.cos(theta[k])-np.cos(theta_obs)*cos_zeta[i,k])/((sin_zeta[i,k])*np.sin(-theta_obs)) #formula for psi 
-    sin_psi[i,k] = math.sqrt(1-(cos_psi[i,k])**2)
+    sin_psi[i,k] = -(np.cos(theta[k])-np.cos(theta_obs)*cos_zeta[i,k])/(sin_zeta[i,k])*np.sin(theta_obs) #formula for psi 
+    cos_psi[i,k] = np.sqrt(1-(sin_psi[i,k])**2)
     x[i,k] = r[i,k]*cos_psi[i,k]
     y[i,k] = r[i,k]*sin_psi[i,k]
     '''
